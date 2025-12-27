3f5beb6349320aff70e75801f1c8669a30312288
James Robinson <github@zed.com.au>
2025-12-27 13:47:09 +1100

    Add Vercel Analytics to the application

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
+import { Analytics } from '@vercel/analytics/react'
 import './globals.css'
 
 const inter = Inter({ subsets: ['latin'] })
@@ -115,6 +116,7 @@ export default function RootLayout({
           dangerouslySetInnerHTML={{ __html: JSON.stringify(organizationData) }}
         />
         {children}
+        <Analytics />
       </body>
     </html>
   )
