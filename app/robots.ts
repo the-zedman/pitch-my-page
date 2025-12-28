2b97536a41d07b01d7bcc893fabfe3d2ca8c78cd
James Robinson <github@zed.com.au>
2025-12-29 00:00:45 +1100

    Allow auth pages to be crawled by removing /auth/ from robots.txt disallow list

--- app/robots.ts
+++ app/robots.ts
@@ -9,7 +9,7 @@ export default function robots(): MetadataRoute.Robots {
         disallow: [
           '/dashboard/',
           '/api/',
-          '/auth/',
+          '/admin/',
           '/test/',
           '/test-ses/',
         ],
@@ -20,7 +20,7 @@ export default function robots(): MetadataRoute.Robots {
         disallow: [
           '/dashboard/',
           '/api/',
-          '/auth/',
+          '/admin/',
           '/test/',
           '/test-ses/',
         ],
